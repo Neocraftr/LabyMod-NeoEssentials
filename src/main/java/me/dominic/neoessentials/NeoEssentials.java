80f83fcc1255748772b4836d9d10ff67bef968b5
Neocraftr <51837582+Neocraftr@users.noreply.github.com>
2021-12-26 20:03:49 +0100

    Release v1.4.1

--- src/main/java/me/dominic/neoessentials/NeoEssentials.java
+++ src/main/java/me/dominic/neoessentials/NeoEssentials.java
@@ -24,7 +24,7 @@ public class NeoEssentials extends LabyModAddon {
 
     public static final String PREFIX = "§8[§2NeoEssentials§8] §7",
                                COMMAND_PREFIX = ".",
-                               VERSION = "1.4.0";
+                               VERSION = "1.4.1";
     
     private static NeoEssentials neoEssentials;
     private Helper helper;
